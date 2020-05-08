7513dcaa94125e44c82eb34ee967880192329dc0
liweitian <liweitian93@outlook.com>
2020-05-08 16:39:23 +0800

    add debounce when user input

--- Composer/packages/extensions/adaptive-form/src/components/fields/RegexIntentField.tsx
+++ Composer/packages/extensions/adaptive-form/src/components/fields/RegexIntentField.tsx
@@ -6,6 +6,7 @@ import { jsx } from '@emotion/core';
 import React, { useState } from 'react';
 import { FieldProps, useShellApi } from '@bfc/extension';
 import { DialogInfo, RegexRecognizer } from '@bfc/shared';
+import debounce from 'lodash/debounce';
 
 import { StringField } from './StringField';
 
@@ -29,7 +30,7 @@ const RegexIntentField: React.FC<FieldProps> = ({ value: intentName, ...rest }) 
   const [localValue, setLocalValue] = useState(getRegexIntentPattern(currentDialog, intentName));
   const handleIntentchange = (pattern): void => {
     setLocalValue(pattern);
-    shellApi.updateRegExIntent(currentDialog.id, intentName, pattern);
+    debounce(() => shellApi.updateRegExIntent(currentDialog.id, intentName, pattern), 300)();
   };
 
   return <StringField {...rest} label={false} value={localValue} onChange={handleIntentchange} />;
